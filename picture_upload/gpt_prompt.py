6fc3367fc526d564acdbe425a96f77f9594fb307
hanchao2407 <hanchao.xu@gmail.com>
2024-01-07 20:29:23 +0100

    added print image size in terminal

--- picture_upload/gpt_prompt.py
+++ picture_upload/gpt_prompt.py
@@ -20,7 +20,9 @@ client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 def resize_image(image_path, max_width=500):
     with Image.open(image_path) as img:
-        print(img.size)
+        print("Image pixels: " + img.size)
+        file_size = os.path.getsize(image_path)
+        print(f"File size: {file_size} bytes")
         # Calculate the height using the aspect ratio
         ratio = max_width / img.width
         new_height = int(img.height * ratio)
